ac19e08f4400d83ed141360762b6ef9b13afd374
Andrew James <mygithubacc3198@gmail.com>
2026-04-29 20:54:52 +0530

    update: configure cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,11 @@ import cardRoutes from "./routes/card.routes.js"
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}))
+
 app.use(express.json())
 
 app.get("/", (_, res) => {
