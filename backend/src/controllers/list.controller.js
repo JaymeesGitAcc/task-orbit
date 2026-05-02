9dd3b70d4f5b2f103fc62c7f3f641bad9cb12735
Andrew James <mygithubacc3198@gmail.com>
2026-05-02 14:55:03 +0530

    feat: add api and route for moving and reordering lists

--- backend/src/controllers/list.controller.js
+++ backend/src/controllers/list.controller.js
@@ -45,19 +45,68 @@ export const getListsByBoard = async (req, res) => {
 export const deleteList = async (req, res) => {
   const { listId } = req.params
   try {
-
     const list = await List.findById(listId)
 
-    if(!list) {
+    if (!list) {
       return sendError(res, 400, "List not found")
     }
 
-    await Card.deleteMany({listId: list._id})
+    await Card.deleteMany({ listId: list._id })
 
     await List.findByIdAndDelete(list._id)
 
     return sendSuccess(res, 200, "List deleted Successfully")
   } catch (error) {
     return sendError(res, 500, `deleteList Error:: `, error.message)
+  }
+}
+
+export const moveList = async (req, res) => {
+  try {
+    const { listId } = req.params
+    const { targetOrder } = req.body
+    const userId = req.user.id
+
+    const list = await List.findOne({ _id: listId, userId })
+
+    if (!list) {
+      return sendError(res, 404, "List not found")
+    }
+
+    const sourceOrder = list.order
+    const boardId = list.boardId
+
+    if (sourceOrder === targetOrder) {
+      return sendSuccess(res, 200, "No change", list)
+    }
+
+    if (sourceOrder < targetOrder) {
+      // moving down
+      await List.updateMany(
+        {
+          boardId,
+          userId,
+          order: { $gt: sourceOrder, $lte: targetOrder },
+        },
+        { $inc: { order: -1 } },
+      )
+    } else {
+      // moving up
+      await List.updateMany(
+        {
+          boardId,
+          userId,
+          order: { $gte: targetOrder, $lt: sourceOrder },
+        },
+        { $inc: { order: 1 } },
+      )
+    }
+
+    list.order = targetOrder
+    await list.save()
+
+    return sendSuccess(res, 200, "List moved", list)
+  } catch (error) {
+    return sendError(res, 500, error.message)
   }
 }
--- backend/src/routes/list.routes.js
+++ backend/src/routes/list.routes.js
@@ -3,6 +3,7 @@ import {
   createList,
   deleteList,
   getListsByBoard,
+  moveList,
 } from "../controllers/list.controller.js"
 import { protect } from "../middlewares/auth.middleware.js"
 
@@ -11,5 +12,6 @@ const listRoutes = express.Router()
 listRoutes.post("/", protect, createList)
 listRoutes.get("/:boardId", protect, getListsByBoard)
 listRoutes.delete("/:listId", protect, deleteList)
+listRoutes.patch("/:listId/move", protect, moveList)
 
 export default listRoutes
