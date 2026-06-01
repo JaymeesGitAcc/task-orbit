bcccbaefde502438ed5cc7642e7c1a5f9ebf1c42
Andrew James <mygithubacc3198@gmail.com>
2026-06-01 13:46:54 +0530

    update axios

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,9 @@ import { useAuthStore } from "@/store/useAuthStore"
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  timeout: 60000,
 })
 
 API.interceptors.request.use((config) => {
